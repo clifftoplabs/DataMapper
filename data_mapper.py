2f4e27fbc40a7e20289130d5ee1b9fa4d7b0436d
Steve Skloot <steve@clifftoplabs.com>
2023-02-07 18:52:42 -0600

    Find each file in the directory

--- data_mapper.py
+++ data_mapper.py
@@ -12,14 +12,19 @@ Options:
 
 import argparse
 import logging
+import os
 from typing import AnyStr
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+def map_file(file_path: AnyStr):
+  print(f"Map: {file_path}")
+
 def map_files_for_directory(directory_name: AnyStr):
-  logger.info(f"Applying map function to all files in directory {directory_name}")
-  return
+  for (root, _, files) in os.walk(directory_name):
+    for file in files:
+      map_file(os.path.join(root, file))
 
 def main():
   # Parse the input arguments to get the run options and directory name
@@ -32,6 +37,7 @@ def main():
   args = arg_parser.parse_args()
 
   # Apply map function to all files in the specified directory
+  logger.info(f"Applying map function to all files in directory {args.directory_name}")
   map_files_for_directory(args.directory_name)
 
 if __name__ == "__main__":
